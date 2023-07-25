ffd741d42ee4190f1fae43d0e280abe06dc29b73
hehenischal <nischallc56@gmail.com>
2023-07-25 06:12:52 +0545

    some minor ui changes so it fits my portfolio

--- script.js
+++ script.js
@@ -7,10 +7,18 @@ const keys = document.querySelectorAll(".key");
 // Helper function to evaluate the arithmetic expression
 function evaluateExpression(expression) {
   try {
-    return eval(expression);
+    // Replace leading zeros with an empty string before evaluation
+    const sanitizedExpression = expression.replace(/^0+(?=\d)/, "");
+    return eval(sanitizedExpression);
   } catch (error) {
     return "Error";
   }
+}
+
+// Helper function to check if the input is a valid number or operator
+function isValidInput(input) {
+  // Use a regular expression to check if the input is a valid number or operator
+  return /^-?\d*\.?\d+|[\+\-\*\/]$/.test(input);
 }
 
 // Helper function to update the output display
@@ -32,8 +40,14 @@ keys.forEach((key) => {
       // Clear the output
       updateOutput("");
     } else {
-      // Append the clicked key content to the current expression
-      updateOutput(currentExpression + keyContent);
+      // Check if the key content is a valid number or operator
+      if (isValidInput(currentExpression + keyContent)) {
+        // Append the clicked key content to the current expression
+        updateOutput(currentExpression + keyContent);
+      } else {
+        // Ignore the key press if the input is not a valid number or operator
+        console.log("Invalid input: Only numbers and operators (+, -, *, /) allowed.");
+      }
     }
   });
 });
